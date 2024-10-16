575537b3fa407e2f697b4506c416890999a13ce3
Yui <yasashiyui@gmail.com>
2024-10-16 23:01:44 +0300

    Initialized the structure

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactCardComponent } from './contact-card/contact-card.component';
+
+
+
+@NgModule({
+  declarations: [ContactCardComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
