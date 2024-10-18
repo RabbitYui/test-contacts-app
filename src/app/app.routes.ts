ea5b0f8d37dc33205457090ed488774da4b29e98
Yui <yasashiyui@gmail.com>
2024-10-18 19:15:24 +0300

    Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { ContactProfileComponent } from './contact-profile/contact-profile.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    redirectTo: '/contact-list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'contact-profile/:id',
+    component: ContactProfileComponent
+  },
+  {
+    path: 'contact-profile',
+    component: ContactProfileComponent
+  },
+  {
+    path: 'contact-list',
+    component: ContactListComponent
+  },
+];
