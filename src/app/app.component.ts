282849abda1704749a67cd2e2e75a8d38dcefa46
Yui <yasashiyui@gmail.com>
2024-10-20 15:05:35 +0300

    Fixed issue with animations and added new global style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ContactListComponent } from './contact-list/contact-list.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, ContactListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { FormlyModule } from '@ngx-formly/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormlyMaterialModule } from '@ngx-formly/material';
 import { AppComponent } from './app.component';
 import { ContactProfileComponent } from './contact-profile/contact-profile.component';
 import { routes } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
+import { CustomValidators } from './utils/custom-validators';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 
 
@@ -20,6 +22,11 @@ import { BrowserModule } from '@angular/platform-browser';
     RouterModule.forRoot(routes),
     ContactListComponent,
     FormlyModule.forRoot({
+      validators: [
+        { name: 'required', validation: Validators.required },
+        { name: 'email', validation: Validators.email },
+        { name: 'phone', validation: CustomValidators.phoneValidator }
+      ],
       validationMessages: [
         { name: 'required', message: 'This field is required' },
         { name: 'email', message: 'Please enter a valid email address' },
@@ -29,6 +36,9 @@ import { BrowserModule } from '@angular/platform-browser';
     ReactiveFormsModule,
     FormlyMaterialModule,
     ContactProfileComponent
+  ],
+  providers: [
+    provideAnimations()
   ],
   bootstrap: [AppComponent]
 })
