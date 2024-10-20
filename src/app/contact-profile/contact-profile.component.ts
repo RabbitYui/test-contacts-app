2df2ea151d4df646cadb2201ad1bb55b9fcb970f
Yui <yasashiyui@gmail.com>
2024-10-20 15:35:35 +0300

    Finished basic contact profile component

--- src/app/contact-profile/contact-profile.component.ts
+++ src/app/contact-profile/contact-profile.component.ts
@@ -1,12 +1,80 @@
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormlyMaterialModule } from '@ngx-formly/material';
+import { FormlyFieldConfig, FormlyModule } from '@ngx-formly/core';
+import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs';
+import { MatButton } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormlyMaterialModule,
+    FormlyModule,
+    MatButton,
+    CommonModule
+  ],
   templateUrl: './contact-profile.component.html',
   styleUrl: './contact-profile.component.scss'
 })
 export class ContactProfileComponent {
+  contactForm: FormGroup = new FormGroup({});
+  formModel = {
+    firstName: '',
+    lastName: '',
+    phoneNumber: '',
+    contactEmail: '',
+  }
+  contactFormFields: FormlyFieldConfig[] = [
+    {
+      key: 'firstName',
+      type: 'input',
+      props: {
+        label: 'First Name',
+        placeholder: 'First Name',
+        required: true
+      },
+      validators: { validation: ['required'] }
+    },
+    {
+      key: 'lastName',
+      type: 'input',
+      props: {
+        label: 'Last Name',
+        placeholder: 'Last Name'
+      }
+    },
+    {
+      key: 'phoneNumber',
+      type: 'input',
+      props: {
+        label: 'Phone Number',
+        placeholder: '+380000000000',
+        required: true
+      },
+      validators: { validation: ['required', 'phone'] }
+    },
+    {
+      key: 'contactEmail',
+      type: 'input',
+      props: {
+        label: 'Email',
+        placeholder: 'Email',
+        required: true
+      },
+      validators: { validation: ['required', 'email'] }
+    }
+  ];
+  contactId: number | null = null;
+  constructor(activatedRoute: ActivatedRoute) {
+    activatedRoute.params.pipe(take(1))
+      .subscribe(p => {this.contactId = p['id']})
+  }
 
+  onSubmit(model: any): void {
+    console.log(model);
+  }
 }
--- src/app/utils/custom-validators.ts
+++ src/app/utils/custom-validators.ts
@@ -2,8 +2,8 @@ import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 export class CustomValidators {
 
-  static phoneValidator(control: AbstractControl): ValidationErrors {
+  static phoneValidator(control: AbstractControl): ValidationErrors | null {
       const validationRegex = new RegExp(/^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/);
-      return validationRegex.test(control.value) ? {'phone': false} : {'phone': true};
+      return validationRegex.test(control.value) ? null : {'phone': true};
   }
 }
