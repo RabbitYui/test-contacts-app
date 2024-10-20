f77d3ee0f0d65e35e1aa4654d3a0ae9b7a9dedd5
Yui <yasashiyui@gmail.com>
2024-10-20 16:50:18 +0300

    Contact card component finished

--- src/app/shared/contact-card/contact-card.component.ts
+++ src/app/shared/contact-card/contact-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Contact } from '../models/contact';
 
 @Component({
   selector: 'app-contact-card',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './contact-card.component.scss'
 })
 export class ContactCardComponent {
+  @Input() contact: Contact | undefined;
+  @Output() delete: EventEmitter<number> = new EventEmitter<number>();
+  @Output() edit: EventEmitter<number> = new EventEmitter<number>();
 
+  get name() {
+    const lastName = this.contact?.lastName ? ` ${this.contact?.lastName}` : '';
+    return this.contact?.firstName + lastName;
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactCardComponent } from './contact-card/contact-card.component';
-
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { MatDivider } from '@angular/material/divider';
+import { MatIconButton } from '@angular/material/button';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 
 
 @NgModule({
   declarations: [ContactCardComponent],
   imports: [
+    CommonModule,
+    MatCard,
+    MatCardContent,
+    MatCardHeader,
+    MatIcon,
+    MatDivider,
+    MatIconButton,
+    MatMenuTrigger,
+    MatMenu,
+    MatMenuItem,
+    MatCardTitle
+  ],
+  exports: [
+    ContactCardComponent,
     CommonModule
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
